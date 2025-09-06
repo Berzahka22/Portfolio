c26af9d5a6ee2f3edfe3708971e9797fee189cd2
Berzahka Bebdanne Benyaamin <berzahkabebdannebenyaamin22@gmail.com>
2025-09-06 05:38:47 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -506,3 +506,28 @@ var swiper = new Swiper(".mySwiper", {
     },
   },
 });
+document.addEventListener("DOMContentLoaded", function () {
+  // Initialise le slider Swiper sur la classe .mySwiper
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: { slidesPerView: 1 },
+      768: { slidesPerView: 2 },
+      1024: { slidesPerView: 3 },
+    },
+  });
+});
